f7149a561374c25f9fd8d15a4c749b499ba38087
Jonathan Morrell <jmorrell@berkeley.edu>
2020-11-25 12:31:39 -0800

    docs version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2020, Jonathan Morrell'
 author = u'Jonathan Morrell'
 
 # The short X.Y version
-version = u'0.0.1'
+version = u'0.0.15'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.1'
+release = u'0.0.15'
 
 
 # -- General configuration ---------------------------------------------------
